7560a5826a60c0cb8fe02d3e2f0d31048d6020c6
josejuanisaac <josejuanisaac@gmail.com>
2019-05-20 03:16:43 -0400

    notifications

---
+++ singlerecord_20190519224247.sql
@@ -0,0 +1,1 @@
+ALTER TABLE `notification_set` ADD `contact_type` VARCHAR(50) NOT NULL AFTER `datum_record_id`;
---
+++ singlerecord_20190520022510.sql
@@ -0,0 +1,1 @@
+ALTER TABLE `non_user_contact` CHANGE `email` `method` VARCHAR(100) CHARACTER SET latin1 COLLATE latin1_swedish_ci NOT NULL;
